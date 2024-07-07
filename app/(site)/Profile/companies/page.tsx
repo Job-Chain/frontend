4ad5b2af298e03c64896269eb7bbc54351fbcbe5
Sebastien <sewelseb@hotmail.com>
2024-07-07 10:22:34 +0200

    add: company profile

--- app/(site)/Profile/companies/page.tsx
+++ app/(site)/Profile/companies/page.tsx
@@ -10,11 +10,11 @@ const Profile = () => {
                 <div className="bg-white shadow rounded-lg p-6">
                   <div className="flex flex-col items-center">
                     <img
-                      src="https://randomuser.me/api/portraits/men/94.jpg"
+                      src="https://tonicteaching.com/wp-content/uploads/2020/09/TonicTeaching_Logo.png"
                       className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0"
                     ></img>
-                    <h1 className="text-xl font-bold">John Doe</h1>
-                    <p className="text-gray-700">Software Developer</p>
+                    <h1 className="text-xl font-bold">Tonic Teaching</h1>
+                    <p className="text-gray-700">Company</p>
                     <div className="mt-6 flex flex-wrap gap-4 justify-center">
                       <a
                         href="#"
@@ -22,44 +22,19 @@ const Profile = () => {
                       >
                         Contact
                       </a>
-                      <a
-                        href="#"
-                        className="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded"
-                      >
-                        Resume
-                      </a>
                     </div>
-                  </div>
-                  <hr className="my-6 border-t border-gray-300" />
-                  <div className="flex flex-col">
-                    <span className="text-gray-700 uppercase font-bold tracking-wider mb-2">
-                      Skills
-                    </span>
-                    <ul>
-                      <li className="mb-2">JavaScript</li>
-                      <li className="mb-2">React</li>
-                      <li className="mb-2">Node.js</li>
-                      <li className="mb-2">HTML/CSS</li>
-                      <li className="mb-2">Tailwind Css</li>
-                    </ul>
                   </div>
                 </div>
               </div>
               <div className="col-span-4 sm:col-span-9">
                 <div className="bg-white shadow rounded-lg p-6">
-                  <h2 className="text-xl font-bold mb-4">About Me</h2>
+                  <h2 className="text-xl font-bold mb-4">About Tonic teaching</h2>
                   <p className="text-gray-700">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    finibus est vitae tortor ullamcorper, ut vestibulum velit
-                    convallis. Aenean posuere risus non velit egestas suscipit.
-                    Nunc finibus vel ante id euismod. Vestibulum ante ipsum
-                    primis in faucibus orci luctus et ultrices posuere cubilia
-                    Curae; Aliquam erat volutpat. Nulla vulputate pharetra
-                    tellus, in luctus risus rhoncus id.
+                  Tonic Teaching is a dynamic company specializing in providing expert educators and comprehensive courses to universities and business schools. With a focus on high-quality instruction, Tonic Teaching connects academic institutions with experienced professionals who bring real-world insights into the classroom. The company’s tailored courses cover a wide range of subjects, designed to meet the specific needs of each educational partner. By fostering innovative and interactive learning environments, Tonic Teaching enhances the educational experience for students and institutions alike. Their commitment to excellence ensures that learners gain practical skills and knowledge essential for their future careers.
                   </p>
 
                   <h3 className="font-semibold text-center mt-3 -mb-2">
-                    Find me on
+                    Find us on
                   </h3>
                   <div className="flex justify-center items-center gap-6 my-6">
                     <a
@@ -147,65 +122,6 @@ const Profile = () => {
                         ></path>
                       </svg>
                     </a>
-                  </div>
-
-                  <h2 className="text-xl font-bold mt-6 mb-4">Experience</h2>
-                  <div className="mb-6">
-                    <div className="flex justify-between flex-wrap gap-2 w-full">
-                      <span className="text-gray-700 font-bold">
-                        Web Developer
-                      </span>
-                      <p>
-                        <span className="text-gray-700 mr-2">
-                          at ABC Company
-                        </span>
-                        <span className="text-gray-700">2017 - 2019</span>
-                      </p>
-                    </div>
-                    <p className="mt-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed finibus est vitae tortor ullamcorper, ut vestibulum
-                      velit convallis. Aenean posuere risus non velit egestas
-                      suscipit.
-                    </p>
-                  </div>
-                  <div className="mb-6">
-                    <div className="flex justify-between flex-wrap gap-2 w-full">
-                      <span className="text-gray-700 font-bold">
-                        Web Developer
-                      </span>
-                      <p>
-                        <span className="text-gray-700 mr-2">
-                          at ABC Company
-                        </span>
-                        <span className="text-gray-700">2017 - 2019</span>
-                      </p>
-                    </div>
-                    <p className="mt-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed finibus est vitae tortor ullamcorper, ut vestibulum
-                      velit convallis. Aenean posuere risus non velit egestas
-                      suscipit.
-                    </p>
-                  </div>
-                  <div className="mb-6">
-                    <div className="flex justify-between flex-wrap gap-2 w-full">
-                      <span className="text-gray-700 font-bold">
-                        Web Developer
-                      </span>
-                      <p>
-                        <span className="text-gray-700 mr-2">
-                          at ABC Company
-                        </span>
-                        <span className="text-gray-700">2017 - 2019</span>
-                      </p>
-                    </div>
-                    <p className="mt-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed finibus est vitae tortor ullamcorper, ut vestibulum
-                      velit convallis. Aenean posuere risus non velit egestas
-                      suscipit.
-                    </p>
                   </div>
                 </div>
               </div>
